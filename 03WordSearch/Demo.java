6e38d70a81aa108bb73e16edcc396e6c7ce759d7
pingx2 <pingx2.mm@gmail.com>
2014-11-22 23:20:18 -0500

    added text file thing

--- 03WordSearch/Demo.java
+++ 03WordSearch/Demo.java
@@ -1,14 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Demo{
     
-     public static void main(String[]args){
+     public static void main(String[]args) throws FileNotFoundException{
 
-    	WordGrid data = new WordGrid(10,10);
-      	
-	data.addWordHorizontal("cat", 5, 2);
-	data.addWordVertical("dog", 1, 1);
-	data.addWordDiagonal("taco", 5, 4);
-	System.out.println(data.toString());
-	
+      
+	 File text = new File("SATwords;A;.txt");
+	 Scanner  in = new Scanner(text);
 
-    }	
+	 Random rand = new Random();
+	 
+	 ArrayList<String> Words = new ArrayList<String>();
+
+	 while(in.hasNext()){
+	     Words.add(in.next());
+	 }
+	 
+	 WordGrid data = new WordGrid(15,15);
+	 
+	 for(int i = 0; i < Words.size(); i++){
+	     if(rand.nextInt(3) == 0){
+		 if(!data.addWordHorizontal(Words.get(i),rand.nextInt(15),rand.nextInt(10))){
+		     data.addWordHorizontal(Words.get(i),rand.nextInt(15),rand.nextInt(10));
+		 }
+	     }
+	     else if(rand.nextInt(3) == 1){
+		 if(!data.addWordVertical(Words.get(i),rand.nextInt(10),rand.nextInt(15))){
+		     data.addWordVertical(Words.get(i),rand.nextInt(10),rand.nextInt(15));
+		 }
+	     }
+	     else if(rand.nextInt(3) == 2){ 
+		 if(!data.addWordDiagonal(Words.get(i),rand.nextInt(10),rand.nextInt(10))){
+		     data.addWordVertical(Words.get(i),rand.nextInt(10),rand.nextInt(15));
+		 }
+	     }
+	 }
+	 
+	 System.out.println(data.toString());
+	 
+	 
+     }	
 }
