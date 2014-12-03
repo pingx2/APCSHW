8f89d15af96f6a332ca8542f4d2ad365bb2cda30
pingx2 <pingx2.mm@gmail.com>
2014-12-02 20:54:56 -0500

    Update Driver.java

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -7,17 +7,6 @@ public class Driver{
      public static void main(String[]args) throws FileNotFoundException{
 
 	 boolean fillRandomLetters = true;
-
-	 /*this is the "NEW SECTION" 
-	  *
-	  *This is where you need to write something in order
-	  *to get the values from args, OR decide to use default values 
-	  */
-	 /* if(args.length != 3 && args.length != 4){
-	     System.out.println("Error. Please use the following format:");
-	     System.out.println("java Driver rows cols randomSeed answers");
-	     }
-	 */
 	 
 	 try{
 	     WordGrid w = new WordGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
